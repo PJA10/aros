84aad0df08b70fdcc29dc7d4ccbd39f9dbd55bd9
PJA10 <alon.w.10@gmail.com>
2021-01-12 18:24:10 +0000

    added handle_mmp_entries_overlap

--- kernel/include/kernel/mm.h
+++ kernel/include/kernel/mm.h
@@ -6,7 +6,12 @@
 #define KERNEL_VIRTUAL_BASE 0xC0000000
 
 void get_memory_map(multiboot_info_t* mbd, unsigned int magic);
-void mm_map_clear_unrecognised(mmap_entry_t *entry, unsigned int mmap_addr, unsigned int mmap_length);
+
+
+void handle_mmap_entries_overlap(mmap_entry_t* entry, unsigned int mmap_addr, unsigned int mmap_length);
+void sort_mmap(mmap_entry_t* entry, unsigned int mmap_addr, unsigned int mmap_length);
+void swap_mmap_entries(mmap_entry_t* first_entry, mmap_entry_t* second_entry);
+void mmap_clear_unrecognised(mmap_entry_t *entry, unsigned int mmap_addr, unsigned int mmap_length);
 void print_mmap(mmap_entry_t *entry, unsigned int mmap_addr, unsigned int mmap_length);
 mmap_entry_t *get_next_mmap_entry(mmap_entry_t *entry);
 
