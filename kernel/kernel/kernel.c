6972443a245e9562b5ef57c058af18b12bd3b903
PJA10 <alon.w.10@gmail.com>
2021-03-13 20:06:46 +0000

    Added Stack Smashing Protector

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #include <driver/tty.h>
 #include <driver/serial.h>
 
 #include <kernel/mm.h>
 
+
 /*
  * TODO: fix the fact that array out of range doesn't get caught
- * Stack Smash Protector
  * Testing and Unit Testing?
  * Running the os on real hardware
  * Hardware Abstraction - Device Management?
  * arch independent isr
  * interrupt driven io / io using DMA
+ * improve format string printing
  * Timers - maybe PIT first?
  * improve kernel heap - tree instead of linked list
+ * randomize Stack Smash Protector
  *
  * keep in mind Code Structuring + Portability
  */
+
 
 void kernel_main(multiboot_info_t* mbd, unsigned int magic) {
 	terminal_initialize();
